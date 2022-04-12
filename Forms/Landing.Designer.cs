21edf955d737e023a183bd4713d5fdfafa6043f4
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2022-04-11 20:38:19 -0700

    working checkpoint

--- Forms/Landing.Designer.cs
+++ Forms/Landing.Designer.cs
@@ -88,13 +88,13 @@
          this.dataGridMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.FileName,
             this.RecordTitle});
-         this.dataGridMain.Dock = System.Windows.Forms.DockStyle.Left;
+         this.dataGridMain.Dock = System.Windows.Forms.DockStyle.Fill;
          this.dataGridMain.Location = new System.Drawing.Point(0, 24);
          this.dataGridMain.Name = "dataGridMain";
          this.dataGridMain.ReadOnly = true;
          this.dataGridMain.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
          this.dataGridMain.RowTemplate.Height = 25;
-         this.dataGridMain.Size = new System.Drawing.Size(382, 426);
+         this.dataGridMain.Size = new System.Drawing.Size(800, 426);
          this.dataGridMain.TabIndex = 5;
          this.dataGridMain.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridMain_CellContentClick_1);
          // 
@@ -112,7 +112,7 @@
          this.RecordTitle.HeaderText = "Title";
          this.RecordTitle.Name = "RecordTitle";
          this.RecordTitle.ReadOnly = true;
-         this.RecordTitle.Width = 54;
+         this.RecordTitle.Width = 692;
          // 
          // Landing
          // 
